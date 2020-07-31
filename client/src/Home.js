8aaac4cdcab6a53ddb2a9982a91bae9589dcee21
Rebecca Borison <borisonr@gmail.com>
2020-07-31 11:16:53 -0400

    logo

--- client/src/Home.js
+++ client/src/Home.js
@@ -12,7 +12,10 @@ const Home = () => {
     <div className="App">
       <header className="App-header">
         <h1>Codenames</h1>
-        {/* <img className="App-logo" src="./logo.svg" /> */}
+        <img
+          className="App-logo"
+          src="https://raw.githubusercontent.com/borisonr/codenames/master/client/src/logo_w.png"
+        />
         <p>
           Play Codenames online across multiple devices on a shapink board. To
           create a new game or join an existing game, enter a game identifier
